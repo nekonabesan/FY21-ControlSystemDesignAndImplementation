a1699e412ad5647298fff0ae4745fbace1483791
nekonabesan <sheepdogs_2@hotmail.com>
2021-12-13 22:36:38 +0900

    課題3の実装

--- kadai_03.m
+++ kadai_03.m
@@ -1,28 +1,28 @@
 clear all
 close all
-global r x0 v0 a0 M Dx G0 Kp A B C D;
+global d r x0 v0 a0 M Dx G0 KP A B C D E;
 M = 0.5;
 Dx = 16.05;
 G0 = 18.39;
 x0 = -0.2;
 v0 = -1;
-Kp = 5;
+KP = 5;
 r = 0.2;
-a0 = (G0*Kp*r - G0*Kp*x0 - Kp*v0)/M;
-u0 = 0;
+d = 0.2;
+a0 = (G0*KP*r - G0*KP*x0 + G0*d - Dx*v0)/M;
 t0 = 0;
 te = 2;
 
 
 
-A = [(G0*Kp)/Dx; (G0*Kp)/M];
-B = [(G0*Kp)/Dx, 0; (G0*Kp)/M, 0];
-C = [0, 0; 0, Dx/M];
-D = [M/Dx; 0];
+A = [(G0*KP)/Dx; (G0*KP)/M];
+B = [(G0*KP)/Dx, 0; (G0*KP)/M, 0];
+C = [G0/Dx; G0/M];
+D = [0, 0; 0, Dx/M];
+E = [M/Dx; 0];
 
 
-
-[t,xi] = ode45('calc02', [t0, te], [x0; v0]);
+[t,xi] = ode45('calc03', [t0, te], [x0; v0]);
 
 
 figure; % グラフウィンドウを開く
